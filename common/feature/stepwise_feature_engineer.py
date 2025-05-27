6c1168a9bcbca4cb360f36b0229016c93da37008
Robert Mertens <rmertens@tudelft.nl>
2025-05-27 18:54:51 +0200

    update stepwise feature engineer to use numpy arrays instead of data accessors, and updated for readability

--- common/feature/stepwise_feature_engineer.py
+++ common/feature/stepwise_feature_engineer.py
@@ -1,107 +1,67 @@
 import pandas as pd
 import numpy as np
-from typing import Callable, List, Dict, Any
-from common.optimization import DataFrameToNumPyAccessor
+from typing import Callable, List, Any
+
+from common.envs.forex_env import AgentDataCol
 
 class StepwiseFeatureEngineer:
+
     def __init__(self, columns: List[str]):
-        self._pipeline_steps: List[Dict[str, Any]] = []
+        self._pipeline_steps: List[Callable[[np.ndarray, int], dict[str, Any]]] = []
         self._columns = columns
     
     def get_columns(self) -> List[str]:
         """
         Get the columns of the dataframe.
-        
-        Returns
-        -------
-        List[str]
-            The columns returned by the pipeline.
         """
         return self._columns
     
     def set_columns(self, columns: List[str]) -> 'StepwiseFeatureEngineer':
         """
         Set the columns of the dataframe.
-        
-        Parameters
-        ----------
-        columns : List[str]
-            The columns returned by the pipeline.
         """
         self._columns = columns
         return self
         
-    def add(self, func: Callable[[DataFrameToNumPyAccessor, int], dict]) -> 'StepwiseFeatureEngineer':
+    def add(self, func: Callable[[np.ndarray, int], dict[str, Any]]) -> 'StepwiseFeatureEngineer':
         """
         Add a step to the pipeline.
-        
-        Parameters
-        ----------
-        func : Callable
-            The function to be applied to the dataframe and index. Changes the dataframe in place.
-            THIS FUNCTION SHOULD NEVER RETURN NaN OR INFINITY.
         """
         self._pipeline_steps.append(func)
-
         return self
-    
-    #return array of float
 
-    def run(self, df: DataFrameToNumPyAccessor, index: int) -> dict:
+    def run(self, arr: np.ndarray, index: int) -> dict[str, Any]:
         """
         Run the pipeline on the given DataFrame.
-        
-        Parameters
-        ----------
-        df : pd.DataFrame
-            The DataFrame to be processed.
-        
-        Returns
-        -------
-        pd.DataFrame
-            The processed DataFrame.
         """
 
         dictionary = {}
 
         for func in self._pipeline_steps:
             # run func
-            temp = func(df, index)
-            if isinstance(temp, dict):
-                dictionary.update(temp)
-            else:
+            temp = func(arr, index)
+            if not isinstance(temp, dict):
                 raise ValueError("Function must return a dictionary.")
-            
-        # for each key in dictionary, check if it is in columns
-        for key in dictionary.keys():
-            if key not in self._columns:
-                # remove key from dictionary
-                del dictionary[key]
-
-        # check all columns in dictionary are in columns
-        for key in dictionary.keys():
-            if key not in self._columns:
-                raise ValueError(f"Key {key} not in columns of the stepwise feature engineer. Fix the function {func} to return a key in the columns.")
-            
-        # check none of the values in dictionary are NaN or infinity
-        for key in dictionary.keys():
-            if pd.isna(dictionary[key]):
-                raise ValueError(f"Value {dictionary[key]} for key {key} is NaN. Fix the function {func} to return a value that is not NaN.")
-            if np.isinf(dictionary[key]):
-                raise ValueError(f"Value {dictionary[key]} for key {key} is infinity. Fix the function {func} to return a value that is not infinity.")
-
+            for key, value in temp.items():
+                if key not in self._columns:
+                    raise ValueError(f"Key {key} returned by {func.__name__} not in columns {self._columns}.")
+                if pd.isna(value):
+                    raise ValueError(f"Value {value} for key {key} returned by {func.__name__} is NaN.")
+                if np.isinf(dictionary[key]):
+                    raise ValueError(f"Value {value} for key {key} returned by {func.__name__} is infinity.")
+            dictionary.update(temp)
 
         return dictionary
         
 
-def calculate_cash_percentage(data_accessor, index):
-        """
-        Calculate the cash to shares ratio.
-        """
-        current_cash = data_accessor[index, 'cash']
-        current_equity = data_accessor[index, 'equity_close']
-        percentage = current_cash / current_equity
-        return {'cash_percentage': percentage}
+def calculate_cash_percentage(data: np.ndarray, index) -> dict[str, Any]:
+    """
+    Calculate the cash to shares ratio.
+    """
+    current_cash = data[index, AgentDataCol.cash]
+    current_equity = data[index, AgentDataCol.equity_close]
+    percentage = current_cash / current_equity
+    return {'cash_percentage': percentage}
 
 
         
