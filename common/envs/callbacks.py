4e15b0a2b93e1a7ff066a8c762b119bff7cd2b4a
Robert Mertens <rmertens@tudelft.nl>
2025-06-01 01:32:54 +0200

    small callback changes

--- common/envs/callbacks.py
+++ common/envs/callbacks.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 
+import logging
 from stable_baselines3.common.callbacks import BaseCallback
 import os
 
 class SaveOnEpisodeEndCallback(BaseCallback):
-    def __init__(self, save_path: str | Path, verbose=0):
+    def __init__(self, save_path: Path, verbose=0):
         super().__init__(verbose)
-        self.save_path = Path(save_path)
+        if save_path.exists() and not save_path.is_dir():
+            raise ValueError(f"{save_path} is not a valid directory.")
+        self.save_path = save_path
         self.save_path.mkdir(parents=True, exist_ok=True)
         self.episode_num = 0
 
@@ -18,5 +21,5 @@ class SaveOnEpisodeEndCallback(BaseCallback):
             filename = self.save_path / f"model_{self.episode_num}_episodes.zip"
             self.model.save(filename)
             if self.verbose > 0:
-                print(f"Saved model at episode {self.episode_num} to {filename}")
+                logging.info(f"Saved model at episode {self.episode_num} to {filename}")
         return True
