6ad1b1109246b5e06c9d1518ee902d7157e717b7
Robert Mertens <rmertens@tudelft.nl>
2025-06-05 23:08:35 +0200

    reorganize saveonepisodecallback for readability

--- common/envs/callbacks.py
+++ common/envs/callbacks.py
@@ -10,23 +10,26 @@ from common.models.utils import save_model_with_metadata
 from common.scripts import circ_slice, render_horz_bar
 
 class SaveOnEpisodeEndCallback(BaseCallback):
-    def __init__(self, save_path: Path, verbose=0):
+    def __init__(self, models_dir: Path, verbose=0):
         super().__init__(verbose)
-        if save_path.exists() and not save_path.is_dir():
-            raise ValueError(f"{save_path} is not a valid directory.")
-        self.save_path = save_path
-        self.save_path.mkdir(parents=True, exist_ok=True)
+        if models_dir.exists() and not models_dir.is_dir():
+            raise ValueError(f"{models_dir} is not a valid directory.")
+        self.models_dir = models_dir
+        self.models_dir.mkdir(parents=True, exist_ok=True)
         self.episode_num = 0
 
     def _on_step(self) -> bool:
         # Check if episode is done
         done_array = self.locals.get("dones")
-        if done_array is not None and any(done_array):
-            self.episode_num += 1
-            filename = self.save_path / f"model_{self.episode_num}_episodes.zip"
-            save_model_with_metadata(self.model, filename)
-            if self.verbose > 0:
-                logging.info(f"Saved model at episode {self.episode_num} to {filename}")
+        if done_array is None:
+            return True
+        if not any(done_array):
+            return True
+        self.episode_num += 1
+        filename = self.models_dir / f"model_{self.episode_num}_episodes.zip"
+        save_model_with_metadata(self.model, filename)
+        if self.verbose > 0:
+            logging.info(f"Saved model at episode {self.episode_num} to {filename}")
         return True
 
 class CoolStatsCallback(BaseCallback):
